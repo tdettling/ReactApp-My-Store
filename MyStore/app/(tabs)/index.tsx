b8619e325b3e3ce3c7227787e23b560642188930
L Dettling <tdettling@tds.net>
2025-03-12 21:18:52 -0400

    nav install

--- MyStore/app/(tabs)/index.tsx
+++ MyStore/app/(tabs)/index.tsx
@@ -1,18 +1,19 @@
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, ScrollView, Modal, ImageBackground } from 'react-native';
 import { useReducer, useState } from 'react';
-import styles from '../../styles';
+import styles from '../../styles'
 import { FlatList } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 
-
-// testing push
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> open react</Text>
-      <StatusBar style="auto"/>
-    </View>
+    <NavigationContainer> 
+      <View style={styles.container}>
+        <Text> open react</Text>
+        <StatusBar style="auto"/>
+      </View>
+    </NavigationContainer>
   )
 
   
